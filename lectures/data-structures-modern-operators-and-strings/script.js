0b4734b41ed1e216a7e337ea709c60a08815edc1
Valentin <steppenwolffenrir@gmail.com>
2026-01-25 22:45:21 +0300

    feat: add some new code into 'behind the scenes'

---
+++ lectures/data-structures-modern-operators-and-strings/script.js
@@ -0,0 +1,155 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights = '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12, close: 22
+    }, fri: {
+      open: 11, close: 23
+    }, sat: {
+      open: 0, // Open 24 hours
+      close: 24
+    }
+  },
+
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+
+  orderDelivery: function({
+                            starterIndex = 1,
+                            mainIndex = 0,
+                            time = '20:00',
+                            address
+                          }) {
+    console.log(`Order received: "${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]}" will be delivered to ${address} at ${time}.`);
+  },
+
+  orderPasta: function(ing1, ing2, ing3) {
+    console.log(`Here is your delicious Pasta with $${ing1}, ${ing2} and ${ing3}`);
+  }
+};
+
+/*const arr = [7, 8, 9];
+
+const newBadArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(newBadArr);
+
+const newGoodArr = [1, 2, ...arr];
+console.log(newGoodArr);
+
+console.log(...newGoodArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+console.log(restaurant.mainMenu);*/
+
+/// Copy array
+
+/*const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy);*/
+
+// Join 2 arrays or more
+
+/*const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);*/
+
+// Iterables: Arrays, Strings, Maps, Sets. NOT Objects
+
+/*const str = 'Valentin';
+const letters = [...str, '', 'S.'];
+console.log(letters);
+console.log(...str);*/
+// console.log(`${...str}, Anashkin`);
+
+
+
+// restaurant.orderDelivery({
+//   time: '22:30', address: 'Via del Sole, 21', mainIndex: 2, starterIndex: 2
+// });
+//
+// restaurant.orderDelivery({
+//   address: 'Via del Sole, 21', starterIndex: 1
+// });
+
+/// Destructing Objects
+
+/*
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName, openingHours: hours, categories: tags
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+*/
+
+/// Mutating variables
+
+/*let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+console.log(obj);
+({ a, b } = obj);
+console.log(a, b);*/
+
+/// Nested objects
+
+/*const { fri: { open: o, close: c } } = openingHours;
+console.log(o, c);*/
+
+/// Destructing Arrays
+/*
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+/!*
+const temp = main;
+main = secondary;
+secondary = temp;
+console.log(main, secondary);
+*!/
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+console.log(restaurant.order(2, 0));
+
+let [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [6, 8]];
+
+// const [i, , j] = nested;
+// console.log([i, j]);
+
+const [i, , [j, k]] = nested;
+console.log([i, j, k]);
+
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+*/
