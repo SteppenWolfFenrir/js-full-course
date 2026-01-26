d4fdc48247d5e2230d95473f0017e1c7d7c78d44
Valentin <steppenwolffenrir@gmail.com>
2026-01-26 23:24:08 +0300

    feat: add  some new code

--- lectures/data-structures-modern-operators-and-strings/script.js
+++ lectures/data-structures-modern-operators-and-strings/script.js
@@ -37,9 +37,105 @@ const restaurant = {
   },
 
   orderPasta: function(ing1, ing2, ing3) {
-    console.log(`Here is your delicious Pasta with $${ing1}, ${ing2} and ${ing3}`);
+    console.log(`Here is your delicious Pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza: function(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
   }
 };
+
+/*restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);*/
+
+/// Nullish: null and undefined (NOT 0 or '')
+/*const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);*/
+
+/// Usa ANY data type, return ANY data type, short-circuiting
+
+/*console.log('--- OR ---');
+console.log(3 || 'Valentin');
+console.log('' || 'Valentin');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);*/
+
+/*console.log('--- AND ---')
+console.log(0 && 'Valentin');
+console.log(8 && 'Valentin');
+
+console.log('Hello' && 23 && null && 'Valentin');*/
+
+/// Practical example
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza('mushrooms', 'spinach');
+// }
+
+/*console.log(restaurant.orderSoup && restaurant.orderSoup('mushrooms', 'spinach'));
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');*/
+
+/*restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');*/
+
+/// SPREAD, because on RIGHT side of =
+/*const arr = [1, 2, ...[3, 4]];
+console.log(arr);*/
+
+
+/// REST, because on LEFT side of =
+/*const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log([a, b, ...others]);
+
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(pizza, risotto, otherFood);*/
+
+/// Objects
+/*const { sat, ...weekDays } = restaurant.openingHours;
+console.log(sat, weekDays);*/
+
+/// Functions
+/*const add = function(...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);*/
+
+/// Spread Operator
+
+/*!*const ingredients = [prompt('Let\'s make pasta! Ingredient 1 ?'),
+  prompt('Ingredient 2 ?'),
+  prompt('Ingredient 3 ?')];*!/
+
+/!*restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients);*!/
+
+/// Objects
+
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const newRestaurantCopy = {...newRestaurant};
+newRestaurantCopy.name = 'Ristorante Roma';
+console.log(newRestaurantCopy);*/
 
 /*const arr = [7, 8, 9];
 
@@ -58,22 +154,19 @@ console.log(restaurant.mainMenu);*/
 
 /// Copy array
 
-/*const mainMenuCopy = [...restaurant.mainMenu];
+/*cost mainMenuCopy = [...restaurant.mainMenu];
 console.log(mainMenuCopy);*/
 
 // Join 2 arrays or more
-
 /*const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 console.log(menu);*/
 
 // Iterables: Arrays, Strings, Maps, Sets. NOT Objects
-
 /*const str = 'Valentin';
 const letters = [...str, '', 'S.'];
 console.log(letters);
 console.log(...str);*/
 // console.log(`${...str}, Anashkin`);
-
 
 
 // restaurant.orderDelivery({
@@ -86,8 +179,7 @@ console.log(...str);*/
 
 /// Destructing Objects
 
-/*
-const { name, openingHours, categories } = restaurant;
+/*const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
 
 const {
@@ -96,8 +188,7 @@ const {
 console.log(restaurantName, hours, tags);
 
 const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log(menu, starters);
-*/
+console.log(menu, starters);*/
 
 /// Mutating variables
 
@@ -108,12 +199,13 @@ console.log(obj);
 ({ a, b } = obj);
 console.log(a, b);*/
 
-/// Nested objects
+/// Nested object
 
 /*const { fri: { open: o, close: c } } = openingHours;
 console.log(o, c);*/
 
 /// Destructing Arrays
+
 /*
 const arr = [2, 3, 4];
 const a = arr[0];
