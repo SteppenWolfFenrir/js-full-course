1cd425b66aa077adfc0c50c34ee95088477f1e2b
Valentin <steppenwolffenrir@gmail.com>
2026-02-05 23:57:17 +0300

    feat: add Set

--- lectures/data-structures-modern-operators-and-strings/script.js
+++ lectures/data-structures-modern-operators-and-strings/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Data needed for a later exercise
+/// Data needed for a later exercise
 const flights = '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
@@ -15,7 +15,7 @@ const openingHours = {
   }
 };
 
-// Data needed for first part of the section
+/// Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -54,8 +54,38 @@ const restaurant = {
   }
 };
 
+/// Set
+
+const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza']);
+console.log(ordersSet);
+
+console.log(new Set('Valentin'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pasta'));
+console.log(ordersSet.has('Beer'));
+
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+console.log(ordersSet);
+
+for (const order of ordersSet) {
+  console.log(order);
+}
+
+// Example
+
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+
+const staffUnique = [...new Set(staff)];
+console.log(staff);
+console.log(staffUnique);
+console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size);
+console.log(new Set('Valentin').size);
+
 /// Property NAMES
-const properties = Object.keys(openingHours);
+/*const properties = Object.keys(openingHours);
 console.log(properties);
 
 let openStr = `We are open on ${properties.length} days: `;
@@ -63,19 +93,19 @@ let openStr = `We are open on ${properties.length} days: `;
 for (const day of properties) {
   openStr += `${day}, `;
   console.log(openStr);
-}
+}*/
 
 /// Property VALUES
-const values = Object.values(openingHours);
-console.log(values);
+/*const values = Object.values(openingHours);
+console.log(values);*/
 
 /// Entire object
-const entries = Object.entries(openingHours);
+/*const entries = Object.entries(openingHours);
 console.log(entries);
 
 for (const [key, { open, close }] of entries) {
   console.log(`On ${key}, we are open at ${open} and close at ${close}`);
-}
+}*/
 
 /// Optional Chaining
 /*console.log(restaurant.openingHours.thu?.open);
